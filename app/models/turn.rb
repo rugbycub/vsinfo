cf7d44341acb894aba75d121919e67e4cad9f417
Charlie Nuttall <charlie@charlienuttall.com>
2015-01-13 22:43:24 -0800

    db

--- app/models/turn.rb
+++ app/models/turn.rb
@@ -1,8 +1,8 @@
 class Turn < ActiveRecord::Base
 	belongs_to :airplane, class_name: "Airplane",
 						 foreign_key: "reg"
-	belongs_to :airport, class_name: "Airport",
-						 foreign_key: "airport"
+	# belongs_to :airport, class_name: "Airport",
+						 # foreign_key: "airport"
 	belongs_to :departure
 	belongs_to :arrival
 	belongs_to :operation
