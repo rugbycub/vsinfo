17b0c60881fad41a2a1dad3938bc6c60714d50c2
Charlie Nuttall <charlie@charlienuttall.com>
2015-01-13 23:13:41 -0800

    db

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,5 +1,5 @@
 class Flight < ActiveRecord::Base
-	self.primary_key = 'flight_number'
+	# self.primary_key = 'flight_number'
 	# has_many :flight_schedules
 	# has_many :adhoc_cancellations, through: :flight_schedules
 	# has_many :departures, primary_key: 'flight_number'
